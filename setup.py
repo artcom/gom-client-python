939577398d320d8c1c9a28b6986446b687c8fbb3
Andreas Marr <andreasm@artcom.de>
2013-05-06 13:03:27 +0200

    excluding tests directory from egg and also removed generated cfg

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name = "gom-client",
     version = "0.1",
-    packages = find_packages(),
+    packages = find_packages(exclude=['tests']),
     scripts = ['gom_client.py']
     )
